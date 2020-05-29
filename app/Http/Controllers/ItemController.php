27da4005b61361c7bd638946147f47d08a634943
Jun Ito <j.style.0519.i@gmail.com>
2020-05-29 18:39:01 +0900

    Add item controller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -14,7 +14,8 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::all();
+        return view('item/index', ['items' => $items]);
     }
 
     /**
