c95896c85e3779078cc51398bdb24acb7189513e
Jun Ito <j.style.0519.i@gmail.com>
2020-05-29 19:52:30 +0900

    showを追加し変更

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -6,16 +6,18 @@
             @foreach ($items as $item)
             <div class="col-md-4 mb-2">
                 <div class="card">
-                    <div class="card-header">{{ $item->name }}</div>
-                    <div class="card-body">
-                        {{ $item->amount }}
-                    </div>
+                  <div class="card-header">
+                    <a href="/item/{{ $item->id }}">{{ $item->name }}</a>
+                </div>
+                <div class="card-body">
+                    {{ $item->amount }}
                 </div>
             </div>
-            @endforeach
         </div>
-        <div class="row justify-content-center">
-            {{ $items->links() }}
-        </div>
+        @endforeach
     </div>
+    <div class="row justify-content-center">
+        {{ $items->appends(['keyword' => Request::get('keyword')])->links() }}
+    </div>
+</div>
 @endsection
