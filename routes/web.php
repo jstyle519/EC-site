f7a11df3e6b2c2c04cb7843b85e4307adf9230e6
Jun Ito <j.style.0519.i@gmail.com>
2020-05-29 19:04:14 +0900

    route/web.php内容変更

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+Route::get('/', 'ItemController@index');
+
 
 Route::get('/home', 'HomeController@index')->name('home');
